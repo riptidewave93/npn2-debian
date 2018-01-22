1226f73f56c51884b5dda281252aa150915019e4
Chris Blake <chrisrblake93@gmail.com>
2018-01-21 20:15:57 -0600

    Add wireless support Add basic wireless support

--- build.sh
+++ build.sh
@@ -286,6 +286,10 @@ rm -f /etc/udev/rules.d/70-persistent-net.rules
 sed -i 's|#PermitRootLogin prohibit-password|PermitRootLogin yes|g' /etc/ssh/sshd_config
 echo 'HWCLOCKACCESS=yes' >> /etc/default/hwclock
 echo 'RAMTMP=yes' >> /etc/default/tmpfs
+apt-get install -y wireless-tools wpasupplicant firmware-brcm80211
+wget http://ftp.us.debian.org/debian/pool/non-free/f/firmware-nonfree/firmware-realtek_20161130-3_all.deb -O /root/firmware-realtek_20161130-3_all.deb
+dpkg -i /root/firmware-realtek_20161130-3_all.deb
+rm /root/firmware-realtek_20161130-3_all.deb
 rm -f third-stage
 EOF
 chmod +x third-stage
