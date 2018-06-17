0bc52e84346e934532b958afde1fa11e54929801
Chris Blake <chrisrblake93@gmail.com>
2018-06-17 09:38:14 -0500

    Update kernel and u-boot Updates for everyone

--- build.sh
+++ build.sh
@@ -25,12 +25,12 @@ linaro_full_version="7.2.1-2017.11"
 
 # U-Boot settings
 uboot_repo="https://github.com/u-boot/u-boot.git"
-uboot_branch="v2018.05"
+uboot_branch="v2018.07-rc1"
 uboot_overlay_dir="u-boot"
 
 # Kernel settings
 kernel_repo="git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git"
-kernel_branch="v4.16.7"
+kernel_branch="v4.16.16"
 kernel_config="nanopi_h5_defconfig" # Global config for all boards
 kernel_overlay_dir="kernel"
 
